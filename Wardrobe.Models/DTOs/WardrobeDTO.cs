e9243699c49ce239a1f94bfb7d56efac1701ac8c
mbucar <mislav.bucar@telegra-europe.com>
2023-09-13 13:28:07 +0200

    ItemType

--- Wardrobe.Models/DTOs/WardrobeDTO.cs
+++ Wardrobe.Models/DTOs/WardrobeDTO.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wardrobe.Models.Models;
 
 namespace Wardrobe.Models.DTOs
 {
@@ -14,7 +15,7 @@ namespace Wardrobe.Models.DTOs
         public int Id { get; set; }
         [Required(ErrorMessage = "Please enter the color")]
         public string Color { get; set; }
-        [Required(ErrorMessage = "Please enter the item type")]
+        [Required(ErrorMessage = "Please select the item type")]
         public string ItemType { get; set; }
         [Required]
         public int Price { get; set; }
---
+++ Wardrobe.Models/Models/ItemTypeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wardrobe.Models.Models
+{
+    public class ItemTypeModel
+    {
+        public List<string> Models { get; set; }
+    }
+}
