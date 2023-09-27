6aa6e6af7931df1ad3075081170c42bc1fa310a4
mbucar <mislav.bucar@telegra-europe.com>
2023-09-27 13:51:55 +0200

    Finished Item Type site

---
+++ Wardrobe/wwwroot/javascript/common.js
@@ -0,0 +1,8 @@
+﻿function focusInputText(element) {
+    setTimeout(function () {
+        var inputElement = document.querySelector(element);
+        if (inputElement) {
+            inputElement.focus();
+        }
+    }, 10);
+}
