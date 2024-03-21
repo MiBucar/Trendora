b269c66fb986a9c92798cdd51feb1745da983668
Swathed Orange <mislavbucar@gmail.com>
2024-03-21 19:35:04 +0100

    Changes to favicon and title

--- Wardrobe/Program.cs
+++ Wardrobe/Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(10);
+    options.IdleTimeout = TimeSpan.FromDays(10);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
