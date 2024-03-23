6beb0adc601112d4d9a88f86416f38c6cd128fdb
Swathed Orange <mislavbucar@gmail.com>
2024-03-23 18:05:41 +0100

    Fixed a problem with filter fetching wrong amount of products

--- Wardrobe.Services/Interfaces/IProductService.cs
+++ Wardrobe.Services/Interfaces/IProductService.cs
@@ -13,11 +13,13 @@ namespace Wardrobe.Services.Interfaces
         public Task<ProductDTO> Update(ProductDTO pDTO);
         public Task<int> Delete(int id);
         public Task<ProductDTO> GetById(int id);
-        public Task<ProductDTO> GetByName(string name);
+        public Task<ProductDTO> GetByGuid(Guid guid);
         public Task<IEnumerable<ProductDTO>> GetAll();
         public Task<IEnumerable<ProductDTO>> SearchByText(string text);
         public Task<IEnumerable<ProductDTO>> SortByPrice(string type);
         public Task<(IEnumerable<ProductDTO>, int)> GetByItemType(string category, int pageNumber, int pageSize);
         public Task<(IEnumerable<ProductDTO>, int)> GetByCollection(CollectionDTO collection, int pageNumber, int pageSize);
+        public Task<(IEnumerable<ProductDTO>, int)> GetByCollectionAndProducts(IEnumerable<ProductDTO> products, CollectionDTO collection, int pageNumber, int pageSize);
+        public Task<IEnumerable<ProductDTO>> GetAllByCollection(CollectionDTO collection);
     }
 }
